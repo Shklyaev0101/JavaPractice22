ee5c22c2c6a08bf8fdee52b667b98d634862633b
shklyaev0101 <ds.shklyaev@gmail.com>
2022-07-21 18:39:16 +0500

    proc210722

--- src/main/java/JavaPractice/Cycles.java
+++ src/main/java/JavaPractice/Cycles.java
@@ -6,6 +6,15 @@ public class Cycles {
         cycleOne();
         cycleTwo();
         cycleThree();
+        cycleFour();
+        cycleFive();
+        cycleSix();
+        cycleSeven();
+        cycleEight();
+        cycleNine();
+        cycleTen();
+        //cycleEleven();
+
     }
 
     public static void cycleOne() {
@@ -16,8 +25,8 @@ public class Cycles {
     public static void cycleTwo() {
         for (int j = 0; j < 10; j++) {
             System.out.println('*');
-            System.out.println(' ');
         }
+        System.out.println(' ');
     }
 
     public static void cycleThree() {
@@ -28,4 +37,109 @@ public class Cycles {
         System.out.println(' ');
     }
 
+    public static void cycleFour() {            //оборачиваем код еще в один цикл
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+        System.out.println(' ');
+    }
+
+    public static void cycleFive() {            //двойной цикл печатает таблицу из "*" размером 6х6, с пробелом между
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println(' ');
+    }
+
+    public static void cycleSix() {             //правая часть таблицы должна состоять из " 0 "
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (j < 3) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println(' ');
+    }
+
+    public static void cycleSeven() {           //сделать так, чтоб по краям квадрата стоял "0", а внутри " * "
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (i == 0 || i == 5 || j == 0 || j == 5) {
+                    System.out.print("0 ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println(' ');
+    }
+
+    public static void cycleEight() {           //таблица в шахматном порядке
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println(" ");
+    }
+
+    public static void cycleNine() {            //половина таблицы 0, вторая половина * и разделить половины диагональю
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (i > j ) {
+                    System.out.print("0 ");
+                } else if (i < j) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println(" ");
+    }
+
+    public static void cycleTen() {               //сделать так чтоб по краям квадрата стоял " * " а в середине пусто
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (i == 0 || i == 5 || j == 0 || j == 5) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println(" ");
+    }
+
+    /*
+    public static void cycleEleven() {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (i == 0 || i == 5 || j == 0 || j == 5) {
+                    System.out.print("* ");
+                } else {
+                    System.out.println(" ");
+                }
+            }
+        }
+    }
+    */
 }
---
+++ src/main/java/JavaPractice/InputOutput.java
@@ -0,0 +1,26 @@
+package JavaPractice;
+
+public class InputOutput {
+    public static void main(String[] args) {
+        les1();
+        les2();
+        //les3();
+        //les4();
+    }
+    /*
+    Написать класс, который выводит на экран надпись "Hello,world!"
+    */
+    public static void les1(){
+        System.out.println("Hello,world!");
+        System.out.println();
+    }
+
+    /*
+    Напишите класс, который выводит на экран надпись: "H-e-l-l-o-,-f-r-i-e-n-d!"
+    */
+    public static void les2(){
+        System.out.println("H-e-l-l-o-,-f-r-i-e-n-d!");
+        System.out.println();
+    }
+
+}
